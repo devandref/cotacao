d15ff1ea21a8e0902c751d94fc4c98d1c4c10823
devandref <yoandredepaula@gmail.com>
2024-02-18 11:18:00 -0300

    include: log na schedulagem

--- src/main/java/io/github/devandref/scheduler/QuotationScheduler.java
+++ src/main/java/io/github/devandref/scheduler/QuotationScheduler.java
@@ -1,7 +1,10 @@
 package io.github.devandref.scheduler;
 
+import io.github.devandref.message.KafkaEvents;
 import io.github.devandref.service.QuotationService;
 import io.quarkus.scheduler.Scheduled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -10,12 +13,15 @@ import javax.transaction.Transactional;
 @ApplicationScoped
 public class QuotationScheduler {
 
+    private final Logger LOG = LoggerFactory.getLogger(KafkaEvents.class);
+
     @Inject
     private QuotationService quotationService;
 
     @Scheduled(every = "35s", identity = "task-job")
     @Transactional
     void scheduler() {
+        LOG.info("-- Executando Scheduler --");
         quotationService.getCurrencyPrice();
     }
 }
